3b896d5b69779e563d97b7d74c8541c27e51b34d
pschakravarthy <sp_us@yahoo.com>
2019-03-03 10:10:33 -0800

    Initial Creation
    Initial creation

---
+++ utils.ps1
@@ -0,0 +1,50 @@
+<#
+.SYNOPSIS
+Helper functions to support other scripts. 
+
+.DESCRIPTION
+Helper functions to allow scripts to zip/unzip, make-pdf using Chrome, and write to application log.
+
+.NOTES
+    Author: PSChakravarthy
+#>
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+function Zip
+{
+    param([string]$zipfile, [string]$srcpath)
+
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($srcpath, $zipfile)
+}
+
+# Helper function to write to application log
+function Write-Log {
+    param([string]$scriptName,
+	  [string]$entryType="Information",
+	  [string]$msg, 
+	  [string]$eventId=1000)
+
+    # Create log source using the script name
+    $logType = "Application"
+    $logExists = ([System.Diagnostics.EventLog]::Exists($logType) -and [System.Diagnostics.EventLog]::SourceExists($scriptName) )
+    if (! $logExists) {
+	New-EventLog -LogName $logType -Source $scriptName | out-null
+    }
+
+    # Write the log event
+    Write-EventLog -LogName $logType -Source $scriptName -EntryType $entryType  -EventID $eventId -Message $msg
+}
+
+function make-pdf {
+    param($html)
+    $pdf = $html -replace ".html",".pdf"
+    $pdfParams =  '--headless --disable-gpu --enable-local-file-accesses --print-to-pdf="$($pdf)" "$($html)"'
+    $chrome = 'c:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+    & $chrome --headless --disable-gpu --enable-local-file-accesses --print-to-pdf="$($pdf)" "$($html)"
+}
